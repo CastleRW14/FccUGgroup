ec8d8a5572a2d81eb4895605a40c0e4a41d1203d
MarcoGitHubAccount <miacobuc@purdue.edu>
2022-02-25 01:10:55 -0500

    Concat updated

--- SkimGenConcat.py
+++ SkimGenConcat.py
@@ -8,7 +8,7 @@ if(len(sys.argv)>2):
 else:
 	targetpath = filepath
 
-os.system('ls ' + filepath + '/skimgenevents*.root.txt>' + targetpath + '/skimgenfilelist.txt')
+os.system('ls ' + filepath + '/skimgen_eeevents*.root.txt>' + targetpath + '/skimgenfilelist.txt')
 
 filelist = open(targetpath + '/skimgenfilelist.txt','r')
 
@@ -20,6 +20,54 @@ for i in filelist:
 
 skimgen = pd.concat(files)
 
-skimgen.to_csv(targetpath + '/skimgendata.txt', index=False)
+skimgen.to_csv(targetpath + '/skimgen_ee_data.txt', index=False)
+
+os.system('rm ' + targetpath + '/skimgenfilelist.txt')
+
+os.system('ls ' + filepath + '/skimgen_e-muevents*.root.txt>' + targetpath + '/skimgenfilelist.txt')
+
+filelist = open(targetpath + '/skimgenfilelist.txt','r')
+
+files = []
+
+for i in filelist:
+	i = i.replace('\n','')
+	files.append(pd.read_csv(i))
+
+skimgen = pd.concat(files)
+
+skimgen.to_csv(targetpath + '/skimgen_e-mu_data.txt', index=False)
+
+os.system('rm ' + targetpath + '/skimgenfilelist.txt')
+
+os.system('ls ' + filepath + '/skimgen_e+muevents*.root.txt>' + targetpath + '/skimgenfilelist.txt')
+
+filelist = open(targetpath + '/skimgenfilelist.txt','r')
+
+files = []
+
+for i in filelist:
+	i = i.replace('\n','')
+	files.append(pd.read_csv(i))
+
+skimgen = pd.concat(files)
+
+skimgen.to_csv(targetpath + '/skimgen_e+mu_data.txt', index=False)
+
+os.system('rm ' + targetpath + '/skimgenfilelist.txt')
+
+os.system('ls ' + filepath + '/skimgen_mumuevents*.root.txt>' + targetpath + '/skimgenfilelist.txt')
+
+filelist = open(targetpath + '/skimgenfilelist.txt','r')
+
+files = []
+
+for i in filelist:
+	i = i.replace('\n','')
+	files.append(pd.read_csv(i))
+
+skimgen = pd.concat(files)
+
+skimgen.to_csv(targetpath + '/skimgen_mumu_data.txt', index=False)
 
 os.system('rm ' + targetpath + '/skimgenfilelist.txt')
